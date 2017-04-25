cb0b1dc20e6819402dc4a9264bdbc59b2e6f514f
DanTNC <fad11204@yahoo.com.tw>
2017-04-25 17:26:32 +0800

    Initial commit

---
+++ app/src/main/java/sparkgo/sparkgo/Tab1fb.java
@@ -0,0 +1,23 @@
+package sparkgo.sparkgo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+/**
+ * Created by Cool on 2017/4/25.
+ */
+
+public class Tab1fb extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fb, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/sparkgo/sparkgo/Tab2yt.java
@@ -0,0 +1,22 @@
+package sparkgo.sparkgo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Cool on 2017/4/25.
+ */
+
+public class Tab2yt extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fb, container, false);
+
+        return rootView;
+    }
+}
